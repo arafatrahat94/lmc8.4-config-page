de9a024d60d017df1bd3d08899932b80a1ef5c18
Arafath <config.gcambytechinfinity1@gmail.com>
2023-03-19 10:46:01 -0700

    new nav bar is added

---
+++ java.js
@@ -0,0 +1,5 @@
+// its for the navigation open and close section
+var hamburger = document.querySelector(".hamburger");
+hamburger.addEventListener("click", function(){
+  document.querySelector("body").classList.toggle("active");
+})
