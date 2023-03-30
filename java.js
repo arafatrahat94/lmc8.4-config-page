04110c9b10db5637af43f2e311a405aa0ecf366e
Arafath <config.gcambytechinfinity1@gmail.com>
2023-03-30 00:33:44 -0700

    flagship bright is added

--- java.js
+++ java.js
@@ -3,3 +3,12 @@ var hamburger = document.querySelector(".hamburger");
 hamburger.addEventListener("click", function(){
   document.querySelector("body").classList.toggle("active");
 })
+// for link password
+function validatePass(){
+    if(document.getElementById('password').value == 'sadesss'){
+        return true;
+    }else{
+        alert('wrong password!!');
+        return false;
+    }
+}
